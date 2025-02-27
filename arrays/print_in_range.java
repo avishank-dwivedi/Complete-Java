7f804dc5db5bd3a8e68bd3427d4f8cdf2f1079db
avishank-dwivedi <avidwivedi011@gmail.com>
2025-02-27 14:19:30 +0530

    !

---
+++ arrays/print_in_range.java
@@ -0,0 +1,71 @@
+package arrays;
+
+public class print_in_range {
+   
+
+
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;   
+            this.right = null;  
+        }
+    }
+    public static Node insert(Node root, int val){
+        if(root == null){
+            root = new Node(val);
+            return root;
+        }
+        if(root.data >val){
+            root.left = insert(root.left, val);
+        }else
+            root.right = insert(root.right, val);
+        return root;
+    } 
+    public static void inorder(Node root){
+        if(root == null){
+            return;
+        }
+        inorder(root.left);
+        System.out.print(root.data + " ");
+        inorder(root.right);
+        
+    }
+
+    public static void printinrange(Node root , int X , int Y){
+        if (root == null){
+            return;   
+        }
+        if(root.data >= X && root.data <= Y){
+            printinrange(root.left, X, Y);
+            System.out.print(root.data + " ");
+            printinrange(root.right, X, Y);
+        }
+        else if(root.data >= Y){
+            printinrange(root.left, X, Y);
+        }
+        else{
+            printinrange(root.right, X, Y);
+        }
+    }
+    public static void main(String args[]){
+        int values[] ={5 , 1, 3 , 4 , 2 , 7};
+        Node root = null;
+
+        for(int i = 0; i < values.length; i++){
+            root = insert(root, values[i]);
+        
+
+        }
+        inorder(root);
+        System.out.println();
+        printinrange(root, 3, 6);
+
+    }
+}
+
+
---
+++ arrays/search_BST.java
@@ -0,0 +1,157 @@
+package arrays;
+
+
+
+// public class search_BST {
+//     static class Node {
+//         int data;
+//         Node left;
+//         Node right;
+
+//         Node(int data) {
+//             this.data = data;
+//             this.left = null;   
+//             this.right = null;  
+//         }
+//     }
+//     public static Node insert(Node root, int val){
+//         if(root == null){
+//             root = new Node(val);
+//             return root;
+//         }
+//         if(root.data >val){
+//             root.left = insert(root.left, val);
+//         }else
+//             root.right = insert(root.right, val);
+//         return root;
+//     } 
+//     public static void inorder(Node root){
+//         if(root == null){
+//             return;
+//         }
+//         inorder(root.left);
+//         System.out.print(root.data + " ");
+//         inorder(root.right);
+        
+//     }
+
+//     public static boolean search(Node root, int key){ // 0(H) time complexity
+//         if(root == null){
+//             return false;
+//         }
+//         if(root.data ==  key){
+//             return true;
+//         }
+//         if(root.data > key){
+//             return search(root.left, key);
+//         }else{
+//         return search(root.right, key);
+//     }
+// }
+//     public static void main(String args[]){
+//         int values[] ={8,5,3,1,4,6,10,11,14};
+//         Node root = null;
+
+//         for(int i = 0; i < values.length; i++){
+//             root = insert(root, values[i]);
+        
+
+//         }
+//         inorder(root);
+//         System.out.println();
+
+//         if(search(root, 7)){
+//             System.out.println("Element found");
+//         }else{
+//             System.out.println("Element not found");
+//         }
+//     }
+// }
+
+
+
+// delete a node from BST
+public class search_BST {
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;   
+            this.right = null;  
+        }
+    }
+    public static Node insert(Node root, int val){
+        if(root == null){
+            root = new Node(val);
+            return root;
+        }
+        if(root.data >val){
+            root.left = insert(root.left, val);
+        }else
+            root.right = insert(root.right, val);
+        return root;
+    } 
+    public static void inorder(Node root){
+        if(root == null){
+            return;
+        }
+        inorder(root.left);
+        System.out.print(root.data + " ");
+        inorder(root.right);   
+    }
+
+    public static Node delete(Node root, int val){
+        if(root == null){
+            return null;
+        }
+        if(root.data > val){
+            root.left = delete(root.left, val);
+        }else if(root.data < val){
+            root.right = delete(root.right, val);
+        }else{// root.data == val
+            //case 1: no child
+            if(root.left == null && root.right == null){
+                return null;
+            }
+            //case 2: one child
+            if(root.left == null){
+                return root.right;
+            }
+            else if (root.right == null){
+                return root.left;
+                
+            }
+            //case 3: two children
+            Node IS = inorderSuccessor(root.right);
+            root.data = IS.data;
+            root.right = delete(root.right, IS.data);
+                
+        }
+                return root;
+    }
+    public static Node inorderSuccessor(Node root){
+        
+        while(root.left != null){
+            root = root.left;
+        }
+        return root;
+    }
+    public static void main(String args[]){
+        int values[] ={5, 1, 3 ,4, 2 ,7};
+        Node root = null;
+
+        for(int i = 0; i < values.length; i++){
+            root = insert(root, values[i]);
+        
+
+        }
+        inorder(root);
+        System.out.println();
+
+        delete(root, 3);
+        inorder(root);
+    }
+}
